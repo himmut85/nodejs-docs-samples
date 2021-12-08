0da5c4434a7d3c3ab312121c4491ac4bca296df0
himmut85 <67488800+himmut85@users.noreply.github.com>
2021-12-08 09:59:39 +0300

    Update index.js

--- run/helloworld/index.js
+++ run/helloworld/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}! - Test`);
+  res.send(`Hello ${name}! - Test 1`);
 });
 
 const port = process.env.PORT || 8080;
